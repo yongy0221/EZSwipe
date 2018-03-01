31bc1ac38593d05c004d7b66a33edec1427dfeea
yoong9627 <yoong9627@gmail.com>
2018-03-01 12:26:05 -0800

    2st

--- app/src/main/java/com/gazua/ezswipe/DiningSelect.java
+++ app/src/main/java/com/gazua/ezswipe/DiningSelect.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,31 +12,39 @@ import com.google.firebase.auth.FirebaseAuth;
 public class DiningSelect extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    public String message="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dining_select);
-
+        TextView textView = findViewById(R.id.status);
+        textView.setText(message);
         mAuth = FirebaseAuth.getInstance();
-
-        findViewById(R.id.signOutButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View view) {
-                mAuth.signOut();
-                startActivity(new Intent(DiningSelect.this, MainActivity.class));
-                Toast.makeText(DiningSelect.this, "Sign Out.", Toast.LENGTH_LONG).show();
-            }
-        });
+    }
+    public void confirmClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), BuyerPrice.class);
+        startActivity(intent);
+    }
+    public void bpClicked(View v){
+        message="@Bplate";
+        TextView textView = findViewById(R.id.status);
+        textView.setText(message);
+    }
+    public void cvClicked(View v){
+        message="@Covel";
+        TextView textView = findViewById(R.id.status);
+        textView.setText(message);
+    }
+    public void ftClicked(View v){
+        message="@Feast";
+        TextView textView = findViewById(R.id.status);
+        textView.setText(message);
+    }
+    public void dnClicked(View v){
+        message="@DeNeve.";
+        TextView textView = findViewById(R.id.status);
+        textView.setText(message);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if (mAuth.getCurrentUser() == null) {
-            finish();
-            startActivity(new Intent(this, MainActivity.class));
-        }
-    }
 }
