d60b81e4d951e32ee625fafa6b1cd03d112a3a8c
David <davidkim0228@g.ucla.edu>
2018-03-01 13:19:35 -0800

    Dining Select Button

--- app/src/main/java/com/gazua/ezswipe/SellerList.java
+++ app/src/main/java/com/gazua/ezswipe/SellerList.java
@@ -2,6 +2,8 @@ package com.gazua.ezswipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SellerList extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SellerList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_list);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(DiningSelect.dining_hall());
     }
+
+
+
 }
