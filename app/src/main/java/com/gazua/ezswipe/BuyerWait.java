7cd4aeea49b3278a002cf575e9990b415f6d2032
David <davidkim0228@g.ucla.edu>
2018-02-28 16:56:14 -0800

    init setup

---
+++ app/src/main/java/com/gazua/ezswipe/BuyerWait.java
@@ -0,0 +1,13 @@
+package com.gazua.ezswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuyerWait extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_wait);
+    }
+}
---
+++ app/src/main/java/com/gazua/ezswipe/SellerList.java
@@ -0,0 +1,13 @@
+package com.gazua.ezswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_list);
+    }
+}
