13bb5c2f1d0521061a0d67b426e9cff6aa4e0b9a
David <davidkim0228@g.ucla.edu>
2018-03-01 02:20:12 -0800

    added reference

--- app/src/main/java/com/gazua/ezswipe/BuyerWait.java
+++ app/src/main/java/com/gazua/ezswipe/BuyerWait.java
@@ -10,6 +10,6 @@ public class BuyerWait extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyer_wait);
-        Toast.makeText(BuyerWait.this, BuyerPrice.push_reference().toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(BuyerWait.this, DiningSelect.push_reference().toString(), Toast.LENGTH_LONG).show();
     }
 }
