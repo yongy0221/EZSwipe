c18dda45c35db573fcc153505e152222841b3723
Yong Bum Yoo <yongy0221@gmail.com>
2018-03-01 17:28:09 -0800

    ListView Done

--- app/src/main/java/com/gazua/ezswipe/SellerList.java
+++ app/src/main/java/com/gazua/ezswipe/SellerList.java
@@ -17,20 +17,24 @@ import com.google.firebase.database.ValueEventListener;
 
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SellerList extends AppCompatActivity {
+    ListView listview;
+    ArrayList<String> li = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_list);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
-
         final List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-//        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listview = (ListView)findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, li);
+        listview.setAdapter(adapter);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference();
@@ -43,29 +47,43 @@ public class SellerList extends AppCompatActivity {
                     String dining_str = (String) child.child("Location").getValue();
                     if(Objects.equals(dining_str, DiningSelect.dining_hall())) {
                         Map<String, String> map = new HashMap<String, String>();
-                        map.put("RID", (String) child.child("RID").getValue());
-                        map.put("Buyer_ID", (String) child.child("Buyer_ID").getValue());
+//                        map.put("RID", (String) child.child("RID").getValue());
+//                        map.put("Buyer_ID", (String) child.child("Buyer_ID").getValue());
                         map.put("Buyer_name", (String) child.child("Buyer_name").getValue());
-                        map.put("Created_at", (String) child.child("Created_at").getValue());
-                        map.put("Location", (String) child.child("Location").getValue());
+//                        map.put("Created_at", (String) child.child("Created_at").getValue());
+//                        map.put("Location", (String) child.child("Location").getValue());
                         map.put("Number", (String) child.child("Number").getValue());
                         map.put("Price", (String) child.child("Price").getValue());
-                        map.put("Seller_name", (String) child.child("Seller_name").getValue());
-                        map.put("Status", (String) child.child("Status").getValue());
-                        map.put("Buyer_ID", (String) child.child("Buyer_ID").getValue());
+//                        map.put("Seller_name", (String) child.child("Seller_name").getValue());
+//                        map.put("Status", (String) child.child("Status").getValue());
+//                        map.put("Buyer_ID", (String) child.child("Buyer_ID").getValue());
 
                         list.add(map);
                     }
                 }
 
-                textView.setText(list.toString());
+                String st = "";
+
+                if(list.size() != 0) {
+                    for(int i = 0; i < list.size(); i++) {
+                        st = list.get(i).get("Buyer_name");
+                        st = st + ",    People: " + list.get(i).get("Number");
+                        st = st + ",    $" + list.get(i).get("Price");
+                        li.add(st);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Toast.makeText(SellerList.this, "Error", Toast.LENGTH_LONG).show();
             }
+
         });
+
+
 
     }
 
